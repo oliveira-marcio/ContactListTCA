827f2d4192c831aceb2caa64fd428faf63c74ae3
Márcio Souza de Oliveira <marciosouzadev@gmail.com>
2023-07-15 15:26:27 +0100

    test: test add contact flow (non-exhaustive)

--- ContactListTCATests/ContactsFeatureTests.swift
+++ ContactListTCATests/ContactsFeatureTests.swift
@@ -42,4 +42,24 @@ final class ContactsFeatureTests: XCTestCase {
             $0.destination = nil
         }
     }
+
+    func testAddFlow_NonExhaustive() async {
+        let store = TestStore(initialState: ContactsFeature.State()) {
+            ContactsFeature()
+        } withDependencies: {
+            $0.uuid = .incrementing
+        }
+        store.exhaustivity = .off
+
+        await store.send(.addButtonTapped)
+        await store.send(.destination(.presented(.addContact(.setName("Blob Jr.")))))
+        await store.send(.destination(.presented(.addContact(.saveButtonTapped))))
+        await store.skipReceivedActions()
+        store.assert {
+            $0.contacts = [
+                Contact(id: UUID(0), name: "Blob Jr.")
+            ]
+            $0.destination = nil
+        }
+    }
 }
